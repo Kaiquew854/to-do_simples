c8ff6697af9e1ce5094f5589d3e1402fbc5a8bf4
kaique winicios <kaiquew@outlook.com>
2023-01-21 21:34:01 -0300

    validando arquivos de upload

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,20 +1,27 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import * as TodoController from '../controllers/todoController'
 import multer from 'multer';
 
 const storageConfig = multer.diskStorage({
-    destination: (req, file, cb)=>{
+    destination: (req, file, cb) => {
         cb(null, './tmp');
     },
 
-    filename: (req, file, cb)=>{
+    filename: (req, file, cb) => {
         let randomName = Math.floor(Math.random() * 9999999);
-        cb(null,`${randomName+Date.now()}.jpg`);
+        cb(null, `${randomName + Date.now()}.jpg`);
     }
-});
+}
+);
 
 const upload = multer({
-    storage: storageConfig
+    storage: storageConfig,
+    fileFilter: (req, file, cb) => { //aceitar apenas os arquivos que eu quiser
+        const allowed: string[] = ['image/jpg', 'image/jpeg', 'image/png'];
+        cb(null, allowed.includes(file.mimetype));
+    },
+    limits: {fieldSize: 1000000} //limite de 1 mb
+
 });
 
 const router = Router();
